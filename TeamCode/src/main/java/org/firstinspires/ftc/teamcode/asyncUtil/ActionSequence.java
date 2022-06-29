935bfeb7578e3c4cfc61b95ee716ae9f36b783fc
Ian <ianmhornblower@gmail.com>
2022-06-28 23:48:42 -0400

    test branch

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/asyncUtil/ActionSequence.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/asyncUtil/ActionSequence.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode.asyncUtil;
 
 import org.firstinspires.ftc.teamcode.asyncUtil.actions.*;
-import org.firstinspires.ftc.teamcode.control.Trajectory;
 import org.firstinspires.ftc.teamcode.hardware.RobotBase;
 import org.firstinspires.ftc.teamcode.hardware.subsystems.DriveTrain;
 import org.firstinspires.ftc.teamcode.math.Pose2D;
@@ -51,36 +50,12 @@ public class ActionSequence {
         actionSequence.add(new EncoderDriveEx(dt, x, y, h, ticks, fieldCentric));
     }
 
-    public void addFollowActualTrajectory(Trajectory traj) {
-        actionSequence.add(new FollowActualTrajectory(dt, traj));
-    }
-
-    public void addFollowHolonomicPurePursuitTrajectory(Trajectory traj, double radius) {
-        actionSequence.add(new FollowHolonomicPurePursuitTrajectory(dt, traj, radius));
-    }
-
-    public void addFollowPurePursuitTrajectory(Trajectory trajectory, double radius) {
-        actionSequence.add(new FollowPurePursuitTrajectory(dt, trajectory, radius));
-    }
-
-    public void addFollowPurePursuitTrajectory(Trajectory trajectory, double radius, boolean reversed) {
-        actionSequence.add(new FollowPurePursuitTrajectory(dt, trajectory, radius, reversed));
-    }
-
     public void addForwardBackwardControl(int ticks) {
         actionSequence.add(new ForwardBackwardControl(dt, ticks));
     }
 
     public void addIMURotate(double heading) {
         actionSequence.add(new IMURotate(dt, heading));
-    }
-
-    public void addRotate(double heading) {
-        actionSequence.add(new Rotate(dt, heading));
-    }
-
-    public void addRunToPosition(Pose2D pos) {
-        actionSequence.add(new RunToPosition(dt, pos));
     }
 
     public void addWait(double seconds) {
